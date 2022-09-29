31a9f0f7bf9bcb4164bd417427ad6f224818ed62
jonatasJS <soaresjonatas398@gmail.com>
2022-09-29 18:03:46 -0300

    asdasd

--- services/api/index.ts
+++ services/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : "http://avisosbackend.us-3.evennode.com/",
+  baseURL: process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : "https://avisos.herokuapp.com",
 });
 
 console.log(process.env.NEXT_PUBLIC_API_URL);
