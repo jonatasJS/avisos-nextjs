96e1470797c873acfd2295a4b47df8bef2b0f11f
jonatasJS <soaresjonatas398@gmail.com>
2022-09-29 16:13:38 -0300

    asdasd

--- services/api/index.ts
+++ services/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://192.168.60.42:4000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://avisosbackend.us-3.evennode.com/",
 });
 
 export default api;
