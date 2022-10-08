459686c7d677c47507c22d6199416b0f54ea6e36
jonatasJS <soaresjonatas398@gmail.com>
2022-10-08 12:55:40 -0300

    asdfsdfsdf

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -4,6 +4,8 @@ export default function Logo() {
     style={{
       marginTop: "-60px",
       marginBottom: "-60px",
+      height: "4rem",
+      width: "auto",
     }}
       className="mx-auto h-48 w-auto"
       src="http://avisos.jonatas.app/logo-sim.png"
