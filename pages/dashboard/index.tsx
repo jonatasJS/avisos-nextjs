1ad82e008bb03e9b794d8fd2acf2c38019beeefd
jonatasJS <soaresjonatas398@gmail.com>
2022-10-10 13:59:38 -0300

    asdasd

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -279,7 +279,7 @@ export default function Dashboard() {
           author="Jônatas"
           countryName="Brasil"
           email="soaresjonatas398@gmail.com"
-          image="/preview_dashboard.png"
+          image="https://api.microlink.io?url=https%3A%2F%2Favisos.jonatas.app&overlay.browser=dark&overlay.background=linear-gradient(225deg%2C%20%23FF057C%200%25%2C%20%238D0B93%2050%25%2C%20%23321575%20100%25)&screenshot=true&meta=false&embed=screenshot.url"
           language="pt-BR"
           phoneNumber="+55 63 9 84707053"
           themeColor="#FF6F21"
